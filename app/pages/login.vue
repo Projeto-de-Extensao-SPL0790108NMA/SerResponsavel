a57db8f639b9386979f4ab6f4bf70f105a24a80b
tiagobrilhante <tiagobrilhantemania@gmail.com>
2025-08-27 09:37:59 -0400

    feat: initial commit

---
+++ app/pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-alert type="info">teste</v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@vueuse/nuxt',
+    'vuetify-nuxt-module',
+    '@nuxtjs/supabase'
+  ]
+})
